c8067854a5b0173297a25a343c8241c9a1e857c8
Murugan Nagarajan <murugan425@gmail.com>
2017-11-05 15:15:32 +0800

    Combining both Params & QueryParams routes into a single observable.

--- hello-angular/src/app/httpgitposts/httpgitposts.component.ts
+++ hello-angular/src/app/httpgitposts/httpgitposts.component.ts
@@ -1,6 +1,8 @@
+import { Observable } from 'rxjs/Observable';
 import { ActivatedRoute } from '@angular/router';
 import { HttpgitpostsService } from './httpgitposts.service';
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'app-httpgitposts',
@@ -13,16 +15,31 @@ export class HttpgitpostsComponent implements OnInit {
   constructor(private route: ActivatedRoute, private service: HttpgitpostsService) { }
 
   ngOnInit() {
-
-    this.route.queryParamMap.subscribe(params => {
-      console.log(params.get('page') + '::::' + params.get('order'));
-    });
-
-    this.service.getAll()
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .subscribe(combResp => {
+      const id_param = combResp[0].get('id');
+      const name_param = combResp[0].get('name');
+      console.log(id_param + '::::' + name_param);
+      const page_queryparam = combResp[1].get('page');
+      const order_queryparam = combResp[1].get('order');
+      console.log(page_queryparam + '::::' + order_queryparam);
+      this.service.getAll()
       .subscribe(followers => {
         this.followers = followers;
         console.log(followers);
       });
+    });
+
+    // this.route.paramMap.subscribe(params => {
+    //   console.log(params.get('id') + '::::' + params.get('username'));
+    // });
+
+    // this.route.queryParamMap.subscribe(params => {
+    //   console.log(params.get('page') + '::::' + params.get('order'));
+    // });
   }
 
 }
