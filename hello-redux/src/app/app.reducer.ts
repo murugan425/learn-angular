f18be6229a08c58c91aa7ea0354412316702ae19
Murugan Nagarajan <murugan425@gmail.com>
2017-11-16 16:14:10 +0800

    Using tassign to do object copy instead of Object.assign.. Enables typesafe of store objects

--- hello-redux/src/app/app.reducer.ts
+++ hello-redux/src/app/app.reducer.ts
@@ -1,5 +1,6 @@
 import { TODOACTIONS, COUNTERACTIONS } from './app.action';
 import { IAppState } from './app.store';
+import { tassign } from 'tassign';
 
 export function rootReducer(state: IAppState, action) {
     switch (action.type) {
@@ -8,14 +9,14 @@ export function rootReducer(state: IAppState, action) {
             action.todo.id = state.todos.length === 0 ? 1 : lasttodoitem.id + 1;
             // Old State should be updated and the New State should be returned
             // Merge the current state with the new state and retun it.
-            return Object.assign({} , state, {
+            return tassign(state, {
                 todos: state.todos.concat(Object.assign({}, action.todo)),
                 lastUpdate: new Date()
             });
         case TODOACTIONS.TOGGLE_TODO:
             const todo = state.todos.find(item => item.id === action.id);
             const index = state.todos.indexOf(todo);
-            return Object.assign({} , state, {
+            return tassign(state, {
                 todos: [
                     ...state.todos.slice(0, index),
                     Object.assign({}, todo, {isCompleted: !todo.isCompleted}),
@@ -25,19 +26,24 @@ export function rootReducer(state: IAppState, action) {
             });
         case TODOACTIONS.REMOVE_TODO:
             // Filter the single todo item which is removed by the user
-            return Object.assign({}, state, {
+            return tassign(state, {
                 todos: state.todos.filter(item => item.id !== action.id),
                 lastUpdate: new Date()
             });
         case TODOACTIONS.REMOVE_ALL_TODOS:
-            return Object.assign({}, state, {
-                todos: [],
-                lastUpdate: new Date()
-            });
+            return tassign(state, {todos: [], lastUpdate: new Date()});
         case COUNTERACTIONS.INCREMENT:
-            return Object.assign({}, state, {count: state.count + 1});
+            // Able to add a new key/property value in the store which is not declared in the store at all.
+            // This conflicts with the Redux Framework design.
+            // Store object should be immutable and typesafe.
+            // return Object.assign({}, state, {count: state.count + 1, newKey: 'New value in store'});
+
+            // So, instead of using Object.assign lets use tassign, it takes only 2 arguments not 3
+            // return Object.assign({}, state, {count: state.count + 1});
+            // Will show compilation error if you try to add any new property
+            return tassign(state, {count: state.count + 1});
         case COUNTERACTIONS.DECREMENT:
-            return Object.assign({}, state, {count: state.count - 1});
+            return tassign(state, {count: state.count - 1});
     }
     return state;
 }
