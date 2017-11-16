5dbdd47d4b83aab4e8d51128bf8e38d0a5d0fdaa
Murugan Nagarajan <murugan425@gmail.com>
2017-11-17 01:30:46 +0800

    New Project base created  to try, implement and learn angular-animations built on top of Web Animations API.

---
+++ hello-animations/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { ZippyComponent } from './zippy/zippy.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    ZippyComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ hello-animations/src/app/todo/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  items: any[] = [
+    'Wash the dishes',
+    'Call the accountant',
+    'Apply for a car insurance'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addItem(input: HTMLInputElement) {
+    this.items.splice(0, 0, input.value);
+    input.value = '';
+  }
+
+  removeItem(item) {
+    const index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+}
