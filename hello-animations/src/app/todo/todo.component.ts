e33bde0c89d76b6c52d4a8c19210348382dad1dd
Murugan Nagarajan <murugan425@gmail.com>
2017-11-17 19:24:46 +0800

    Refactoring by moving the state change to a single transition

--- hello-animations/src/app/todo/todo.component.ts
+++ hello-animations/src/app/todo/todo.component.ts
@@ -9,11 +9,8 @@ import { trigger, state, transition,  style, animate } from '@angular/animations
     trigger('fade', [
       state('void', style({ opacity: 0 })),
 
-      transition('void => *', [
+      transition('void => *, * => void', [
         animate(2000)
-      ]),
-      transition('* => void', [
-        animate(1000)
       ])
     ])
   ]
