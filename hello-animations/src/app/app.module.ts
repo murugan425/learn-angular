fbe2ea390ba8a726aa3c5e69c0752d2939f4995f
Murugan Nagarajan <murugan425@gmail.com>
2017-11-17 18:45:09 +0800

    Adding the required animation dependency in appmodule. Also, modified the polyfills for lower browser compatibility.

--- hello-animations/src/app/app.module.ts
+++ hello-animations/src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
@@ -14,7 +14,8 @@ import { ZippyComponent } from './zippy/zippy.component';
     ZippyComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
