571b042703310bce1c04e052f6e3006ca7aeb285
Murugan Nagarajan <murugan425@gmail.com>
2017-11-18 15:39:17 +0800

    Converting the millisecond number format into a string seconds format 'easier to understand '

--- hello-animations/src/app/animations.ts
+++ hello-animations/src/app/animations.ts
@@ -3,16 +3,16 @@ import { trigger, state, transition,  style, animate } from '@angular/animations
 export let fade = trigger('fade', [
     state('void', style({ opacity: 0 })),
     transition(':enter, :leave', [
-        animate(2000)
+        animate('0.5s') // FYI: 500 = 0.5s
     ])
 ]);
 
 export let slide = trigger('slide', [
     transition(':enter', [
         style({ transform: 'translateX(-10px)'}),
-        animate(500)
+        animate('0.5s')
     ]),
     transition(':leave', [
-        animate(500, style({ transform: 'translateX(-50%)'}))
+        animate('0.5s', style({ transform: 'translateX(-50%)'}))
     ])
 ]);
