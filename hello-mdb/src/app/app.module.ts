f9768bc242aa6388da1290fc2ad8a8788998808b
Murugan Nagarajan <murugan425@gmail.com>
2019-01-22 17:28:13 +0800

    Adding material design to the application.

--- hello-mdb/src/app/app.module.ts
+++ hello-mdb/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './corporate/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
