f6598f032a51167c761831f3bfa321d7bcfa2c60
Murugan Nagarajan <murugan425@gmail.com>
2019-02-21 00:21:35 +0800

    start creating the components.

--- app-shopping/src/app/app.module.ts
+++ app-shopping/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShopNavbarComponent } from './shop-navbar/shop-navbar.component';
+import { ShopHomeComponent } from './shop-home/shop-home.component';
+import { ShopProductsComponent } from './shop-products/shop-products.component';
+import { ShopCartComponent } from './shop-cart/shop-cart.component';
+import { ShopOrdersComponent } from './shop-orders/shop-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShopNavbarComponent
+    ShopNavbarComponent,
+    ShopHomeComponent,
+    ShopProductsComponent,
+    ShopCartComponent,
+    ShopOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
   ],
   imports: [
     BrowserModule,
