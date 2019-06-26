8c6a6c9700349641d0b30954b10e0944c53ed4d3
Murugan Nagarajan <murugan425@gmail.com>
2019-06-27 00:09:48 +0800

    inline templates & styles in component

--- hello-ang8/src/app/servers/servers.component.ts
+++ hello-ang8/src/app/servers/servers.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // templateUrl: './servers.component.html',
+  template: '<p>servers duplicated!</p> 1.<app-server></app-server><br/>2.<app-server></app-server>',
+  // styleUrls: ['./servers.component.css']
+  styles: [' app-server  {color: green;}']
 })
 export class ServersComponent implements OnInit {
 
