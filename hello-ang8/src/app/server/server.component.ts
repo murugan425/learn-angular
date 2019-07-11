f989117785bc123d0bc607d1cec1ed15fd5a2ca9
Murugan Nagarajan <murugan425@gmail.com>
2019-07-11 18:32:53 +0800

    Updated the css styles of the components

--- hello-ang8/src/app/server/server.component.ts
+++ hello-ang8/src/app/server/server.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
+    styles: [' h3  {color: green;}']
 })
 export class ServerComponent {
 
