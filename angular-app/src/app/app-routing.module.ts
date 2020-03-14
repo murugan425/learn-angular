43a21024346aeccee183f89906b00f1b0af01bde
murugan425 <murugan425@gmail.com>
2020-03-15 00:02:29 +0530

    Added angular material and updated the routing. Routes validated with new components. Works fine

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckboxComponent } from './material/checkbox/checkbox.component';
+import { HomeComponent } from './components/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'checkbox', component: CheckboxComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CheckboxComponent } from './material/checkbox/checkbox.component';
+import { HomeComponent } from './components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckboxComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
