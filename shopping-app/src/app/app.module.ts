5aea62d9f124b7c07fec7b4424fe8c2f2a4701d8
murugan425 <murugan425@gmail.com>
2020-03-17 15:24:47 +0530

    Create the module & core components.,[Recipes,Shopping,Header,Footer]

--- shopping-app/src/app/app.module.ts
+++ shopping-app/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { RecipesComponent } from './modules/components/recipes/recipes.component';
+import { RecipesListComponent } from './modules/components/recipes/recipes-list/recipes-list.component';
+import { RecipesDetailsComponent } from './modules/components/recipes/recipes-details/recipes-details.component';
+import { ShoppingComponent } from './modules/components/shopping/shopping.component';
+import { ShoppingListComponent } from './modules/components/shopping/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './modules/components/shopping/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesDetailsComponent,
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
