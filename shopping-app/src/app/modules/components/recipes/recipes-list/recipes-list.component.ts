69ede553035d899f56c2506c4d11e740f1622327
murugan425 <murugan425@gmail.com>
2020-03-18 10:21:50 +0530

    Recipe list created..,

--- shopping-app/src/app/modules/components/recipes/recipes-list/recipes-list.component.ts
+++ shopping-app/src/app/modules/components/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [];
+
+  constructor() {
+    this.recipes.push(
+      new Recipe('Hyderabad Biryani', 'Easy Recipe for a spicy Biryani',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Hyderabadi_Chicken_Biryani.jpg/1024px-Hyderabadi_Chicken_Biryani.jpg'),
+      new Recipe('Pulihora', 'Pulihora is prepared with tamarind and jaggery',
+        'https://aahaaramonline.com/wp-content/uploads/2019/04/Andhra_Nuvvula_Chintapandu_Pulihora_1.jpg'))
+  }
 
   ngOnInit(): void {
   }
