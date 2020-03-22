b567ded3b9ba0033e8eaf9158e3035c167c5bfc1
Murugan Nagarajan <murugan425@gmail.com>
2020-03-22 13:54:25 +0530

    Using @Hostbinding in directives.

--- angular-app/src/app/directives/bgblue.directive.ts
+++ angular-app/src/app/directives/bgblue.directive.ts
@@ -1,9 +1,12 @@
-import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBgblue]'
 })
 export class BgblueDirective implements OnInit {
+
+  @HostBinding('style.background') bgColor: string;
+  @HostBinding('style.color') color: string;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
@@ -12,12 +15,16 @@ export class BgblueDirective implements OnInit {
   }
 
   @HostListener('mouseenter') mousehover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', '#cae9f9');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#012bc7');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', '#cae9f9');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', '#012bc7');
+    this.bgColor = '#cae9f9';
+    this.color = '#012bc7';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#333');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', '#333');
+    this.bgColor = 'transparent';
+    this.color = '#333';
   }
 }
