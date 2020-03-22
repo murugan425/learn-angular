e53363298d2dc5efb136649f5df9afc32dbb17df
Murugan Nagarajan <murugan425@gmail.com>
2020-03-22 13:26:03 +0530

    Changing styles on mouse hover using @Hostlistener directives

--- angular-app/src/app/directives/bgblue.directive.ts
+++ angular-app/src/app/directives/bgblue.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBgblue]'
@@ -8,8 +8,16 @@ export class BgblueDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.renderer.appendChild(this.elRef.nativeElement, this.renderer.createComment('Comment Rendered'));
+  }
+
+  @HostListener('mouseenter') mousehover(eventData: Event) {
     this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', '#cae9f9');
     this.renderer.setStyle(this.elRef.nativeElement, 'color', '#012bc7');
-    this.renderer.appendChild(this.elRef.nativeElement, this.renderer.createComment('Comment Rendered'));
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#333');
   }
 }
