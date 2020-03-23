5d5c177ad8dcbf9475cc4ae6886dab7f8c32bf86
Murugan Nagarajan <murugan425@gmail.com>
2020-03-23 19:54:19 +0530

    Property binding in directives.,

--- angular-app/src/app/directives/bgblue.directive.ts
+++ angular-app/src/app/directives/bgblue.directive.ts
@@ -8,6 +8,9 @@ export class BgblueDirective implements OnInit {
   @HostBinding('style.background') bgColor: string;
   @HostBinding('style.color') color: string;
 
+  @Input() defaultColorValue = 'transparent';
+  @Input() bgColorValue: string;
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
@@ -17,14 +20,14 @@ export class BgblueDirective implements OnInit {
   @HostListener('mouseenter') mousehover(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', '#cae9f9');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', '#012bc7');
-    this.bgColor = '#cae9f9';
+    this.bgColor = this.bgColorValue;
     this.color = '#012bc7';
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'transparent');
     // this.renderer.setStyle(this.elRef.nativeElement, 'color', '#333');
-    this.bgColor = 'transparent';
+    this.bgColor = this.defaultColorValue;
     this.color = '#333';
   }
 }
