c5e0501e1273a4c01e7efa6330e995c6b5df5287
Murugan Nagarajan <murugan425@gmail.com>
2020-04-02 00:29:36 +0530

    destroy the subscription when the component is destroyed.

--- angular-app/src/app/routing/user/user.component.ts
+++ angular-app/src/app/routing/user/user.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { User } from 'src/app/common/model/user.model';
 import { UsersService } from 'src/app/services/users.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
 
   user: User;
+  routeParamSubscription: Subscription;
 
   constructor(private userService: UsersService,
               private route: ActivatedRoute) { }
@@ -20,10 +22,15 @@ export class UserComponent implements OnInit {
 
     // This will make sure that the component variable is updated whenever the params changes
     // without re-initialization of the component
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.user = this.userService.getUserByName(params.name);
-    });
+    this.routeParamSubscription =  this.route.params.subscribe(
+                    (params: Params) => {
+                      this.user = this.userService.getUserByName(params.name);
+                    });
+  }
 
+  // Angular by default removes the subscriptions from memory.
+  // It's always preferrable to destroy the subscription when the component is destroyed.
+  ngOnDestroy(): void {
+    this.routeParamSubscription.unsubscribe();
   }
 }
