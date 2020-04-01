cbaa66b4f81010a9bf84c0ef7a42f0032b4fc28a
Murugan Nagarajan <murugan425@gmail.com>
2020-04-02 00:21:21 +0530

    reactively listening to the route parameters using angular subscribe feature

--- angular-app/src/app/routing/user/user.component.ts
+++ angular-app/src/app/routing/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'src/app/common/model/user.model';
 import { UsersService } from 'src/app/services/users.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,7 +16,14 @@ export class UserComponent implements OnInit {
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.user = this.userService.getUserByName(this.route.snapshot.params.name);
-  }
+    // this.user = this.userService.getUserByName(this.route.snapshot.params.name);
 
+    // This will make sure that the component variable is updated whenever the params changes
+    // without re-initialization of the component
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.user = this.userService.getUserByName(params.name);
+    });
+
+  }
 }
