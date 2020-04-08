acbcea340a28e2fc312bab3aef94305f1cc8c64f
Murugan Nagarajan <murugan425@gmail.com>
2020-04-08 22:30:19 +0530

    Custom observables - Observer : Data, Error & Complete handlers. Rxjs Operators like Filter, Map implemented prior to the handlers.

--- angular-app/src/app/components/observables/observables.component.ts
+++ angular-app/src/app/components/observables/observables.component.ts
@@ -1,24 +1,94 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
   styleUrls: ['./observables.component.scss']
 })
-export class ObservablesComponent implements OnInit {
+export class ObservablesComponent implements OnInit, OnDestroy {
 
-  @Input() counter: number;
+  @Input() intervalCounter: number;
+
+  @Input() customCouter: any;
+
+  private intervalSubscription: Subscription;
+
+  private customSubscription: Subscription;
+
+  private customObservables: Observable<number>;
+
+  private customObservableOperator: Observable<string>;
 
   constructor() { }
 
   ngOnInit(): void {
-    interval(1000).subscribe(
+    this.intervalSubscription = interval(1000).subscribe(
       count => {
-        console.log(count);
-        this.counter = count;
+        // console.log('INTERVAL: ' + count);
+        this.intervalCounter = count;
       }
     );
+
+    this.customObservables = new Observable(subscriber => {
+      let customcount = 0;
+      setInterval(() => {
+        subscriber.next(customcount);
+        customcount = customcount + 1;
+        console.log('OBSERVABLE: ' + 'CUSTOM COUNTER: ' + customcount);
+        if ( customcount > 90) {
+          subscriber.error(new Error('Custom observable is destroyed after 10 secs'));
+          console.log('OBSERVABLE: ' + 'CUSTOM COUNTER ERROR ' + customcount);
+        }
+        if ( customcount > 100) {
+          subscriber.complete();
+          console.log('OBSERVABLE: ' + 'CUSTOM COUNTER COMPLETED ' + customcount);
+        }
+      }, 500);
+
+      /*
+      setInterval(() => {
+        console.log('ERROR IN CUSTOM COUNTER : ' + customcount);
+        subscriber.error(new Error('Custom observable is destroyed after 10 secs'));
+      }, 5000);
+
+      setInterval(() => {
+        console.log('COMPLETE CUSTOM COUNTER : ' + customcount);
+        subscriber.complete();
+      }, 10000);
+      */
+    });
+
+    // .create is depreciated
+    /*
+    this.customObservables = Observable.create((observer) => {
+      let count = 10;
+      setInterval(() => {
+        observer.next(count);
+        count = count + 10;
+      }, 1000);
+    });
+    */
+
+    this.customObservableOperator = this.customObservables.pipe(filter(data =>(data % 3 === 0)), map((data: number) => {
+      return ' DATACOUNT: ' + data;
+    }));
+
+    this.customSubscription = this.customObservableOperator.subscribe(count => {
+      this.customCouter = count;
+      console.log('OBSERVER: ' + 'CUSTOM COUNTER: ' + count);
+    }, error => {
+      console.log(error.message + ' occurred. So the observer is unscubscribed');
+      // this.customSubscription.unsubscribe();
+    }, () => {
+        console.log('OBSERVER: ' + 'CUSTOM COUNTER COMPLETED');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.intervalSubscription.unsubscribe();
+    this.customSubscription.unsubscribe();
   }
 
 }
